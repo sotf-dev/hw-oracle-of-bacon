e1f7c06ce5c6ad5a48e26612211268a5bd367fde
Luis Solorzano <luisnic20@gmail.com>
2018-12-10 21:42:03 -0600

    Oracle  of bacon

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -12,7 +12,7 @@ class OracleOfBacon
 
   attr_accessor :from, :to
   attr_reader :api_key, :response, :uri
-  
+
   include ActiveModel::Validations
   validates_presence_of :from
   validates_presence_of :to
@@ -21,10 +21,15 @@ class OracleOfBacon
 
   def from_does_not_equal_to
     # YOUR CODE HERE
+    @errors.add(@from, @to, message: "Shouldn't be the same") if @from == @to
   end
 
   def initialize(api_key='')
     # your code here
+    @api_key = api_key
+    @errors = ActiveModel::Errors.new(self)
+    @from = "Kevin Bacon"
+    @to = "Kevin Bacon"
   end
 
   def find_connections
@@ -37,15 +42,22 @@ class OracleOfBacon
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
+      raise NetworkError.new(e.message)
     end
     # your code here: create the OracleOfBacon::Response object
+    OracleOfBacon::Response.new(xml)
   end
 
   def make_uri_from_arguments
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
+
+    #params =  "p=fake_key&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
+    params =  "p=#{@api_key}&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?#{params}"
+
   end
-      
+
   class Response
     attr_reader :type, :data
     # create a Response object from a string of XML markup.
@@ -61,12 +73,32 @@ class OracleOfBacon
         parse_error_response
       # your code here: 'elsif' clauses to handle other responses
       # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      # object should have type 'unknown' and data 'unknown response'
+      elsif ! @doc.xpath('/link').empty?
+        @type = :graph
+        parse_xml_xpath('/link/*')
+
+      elsif ! @doc.xpath('/spellcheck').empty?
+        @type = :spellcheck
+        parse_xml_xpath('/spellcheck/*')
+
+      else
+        @type = :unknown
+        @data = "unknown"
       end
+
+
     end
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
+    end
+
+    def parse_xml_xpath(xpath)
+      @data = []
+      @doc.xpath(xpath).each do |element|
+        @data.push(element.text)
+      end
     end
   end
 end
