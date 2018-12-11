d31191e96b37d5396859b29a0bf75ebf93ffdf6d
Luis Solorzano <luisnic20@gmail.com>
2018-12-10 22:05:41 -0600

    reformat code

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -1,14 +1,17 @@
-require 'byebug'                # optional, may be helpful
-require 'open-uri'              # allows open('http://...') to return body
-require 'cgi'                   # for escaping URIs
-require 'nokogiri'              # XML parser
-require 'active_model'          # for validations
+require 'byebug' # optional, may be helpful
+require 'open-uri' # allows open('http://...') to return body
+require 'cgi' # for escaping URIs
+require 'nokogiri' # XML parser
+require 'active_model' # for validations
 
 class OracleOfBacon
 
-  class InvalidError < RuntimeError ; end
-  class NetworkError < RuntimeError ; end
-  class InvalidKeyError < RuntimeError ; end
+  class InvalidError < RuntimeError;
+  end
+  class NetworkError < RuntimeError;
+  end
+  class InvalidKeyError < RuntimeError;
+  end
 
   attr_accessor :from, :to
   attr_reader :api_key, :response, :uri
@@ -24,7 +27,7 @@ class OracleOfBacon
     @errors.add(@from, @to, message: "Shouldn't be the same") if @from == @to
   end
 
-  def initialize(api_key='')
+  def initialize(api_key = '')
     # your code here
     @api_key = api_key
     @errors = ActiveModel::Errors.new(self)
@@ -37,8 +40,8 @@ class OracleOfBacon
     begin
       xml = URI.parse(uri).read
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-      Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
-      Net::ProtocolError => e
+        Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
+        Net::ProtocolError => e
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
@@ -52,9 +55,7 @@ class OracleOfBacon
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
 
-    #params =  "p=fake_key&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
-    params =  "p=#{@api_key}&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
-    @uri = "http://oracleofbacon.org/cgi-bin/xml?#{params}"
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?p=#{@api_key}&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
 
   end
 
@@ -69,16 +70,16 @@ class OracleOfBacon
     private
 
     def parse_response
-      if ! @doc.xpath('/error').empty?
+      if !@doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'
-      elsif ! @doc.xpath('/link').empty?
+        # your code here: 'elsif' clauses to handle other responses
+        # for responses not matching the 3 basic types, the Response
+        # object should have type 'unknown' and data 'unknown response'
+      elsif !@doc.xpath('/link').empty?
         @type = :graph
         parse_xml_xpath('/link/*')
 
-      elsif ! @doc.xpath('/spellcheck').empty?
+      elsif !@doc.xpath('/spellcheck').empty?
         @type = :spellcheck
         parse_xml_xpath('/spellcheck/*')
 
@@ -89,6 +90,7 @@ class OracleOfBacon
 
 
     end
+
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
